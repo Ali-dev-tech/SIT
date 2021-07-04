ca81ab616b95e3f38a943df5b2b56750957ce791
Ali-dev-tech <aliabbassalaf01@gmail.com>
2021-07-04 21:42:15 +0400

    HOMEPAGE

--- Homepage/home.js
+++
@@ -1,9 +0,0 @@
-var burger = document.querySelector('.burger')
-console.log(burger)
-var links = document.querySelector('.side-bar')
-console.log(links)
-
-burger.addEventListener('click',function(){
-    links.classList.toggle('left')
-    burger.classList.toggle("is-active")
-})
